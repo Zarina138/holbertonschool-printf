ad7ccbb9e38a5b33a6c649ec760ee35a16a382d5
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-30 01:37:18 +0000

    printf-format

--- printf.c
+++ printf.c
@@ -1,44 +1,50 @@
 #include "main.h"
 
+/**
+ * _printf - produces output according to a format
+ * @format: a character string with format specifiers
+ *
+ * Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    va_list zg;
-    int i = 0, count = 0;
+	va_list zg;
+	int i = 0, count = 0;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(zg, format);
+	va_start(zg, format);
 
-    while (format && format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (!format[i])
-                return (-1);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i])
+				return (-1);
 
-            if (format[i] == 'c')
-                count += char_print(va_arg(zg, int));
-            else if (format[i] == 's')
-                count += string_print(va_arg(zg, char *));
-            else if (format[i] == '%')
-                count += percent_print();
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-                count += 2;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
+			if (format[i] == 'c')
+				count += char_print(va_arg(zg, int));
+			else if (format[i] == 's')
+				count += string_print(va_arg(zg, char *));
+			else if (format[i] == '%')
+				count += percent_print();
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
 
-    va_end(zg);
-    return (count);
+	va_end(zg);
+	return (count);
 }
